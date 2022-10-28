f1969c2001b374ef03aadd99d8615b8c8019eea5
Matheus dos Reis Casarim <37851549+mathues423@users.noreply.github.com>
2022-10-27 22:18:00 -0300

    Add files via upload

--- Factorymethod/src/Teste/ServicoPedirItemTest.java
+++ Factorymethod/src/Teste/ServicoPedirItemTest.java
@@ -1,7 +1,22 @@
 package Teste;
 
+import Main.FabricaServico;
+import Main.LServico;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServicoPedirItemTest {
+    @Test
+    void deveTestarAdicao() {
 
+        LServico servico = FabricaServico.obterServico("PedirItem");
+        assertEquals("Pedido aceito", servico.adicionar());
+    }
+
+    @Test
+    void deveTestarCancela() {
+        LServico servico = FabricaServico.obterServico("PedirItem");
+        assertEquals("Pedido de cancelamento do item do crdapio aceito", servico.pedirCancelamento());
+    }
 }
--- Factorymethod/src/Teste/ServicoTempoInternetTest.java
+++ Factorymethod/src/Teste/ServicoTempoInternetTest.java
@@ -1,7 +1,22 @@
 package Teste;
 
+import Main.FabricaServico;
+import Main.LServico;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ServicoTempoInternetTest {
+    @Test
+    void deveTestarAdicao() {
 
+        LServico servico = FabricaServico.obterServico("TempoInternet");
+        assertEquals("Tempo de internet adicionado", servico.adicionar());
+    }
+
+    @Test
+    void deveTestarCancela() {
+        LServico servico = FabricaServico.obterServico("TempoInternet");
+        assertEquals("Pedido de cancelamento de tempo de internet aceito", servico.pedirCancelamento());
+    }
 }
